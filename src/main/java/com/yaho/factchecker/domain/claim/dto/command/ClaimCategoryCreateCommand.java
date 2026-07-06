00aa977e0d733e6031b529f079c541c2b2a4e1b2
chanhongkwak <rhkrcksghd1@gmail.com>
2026-07-06 18:28:31 +0900

    사용자 주장 정제 오케스트라 구현

--- src/main/java/com/yaho/factchecker/domain/claim/dto/command/ClaimCategoryCreateCommand.java
+++ src/main/java/com/yaho/factchecker/domain/claim/dto/command/ClaimCategoryCreateCommand.java
@@ -1,7 +1,9 @@
 package com.yaho.factchecker.domain.claim.dto.command;
 
 import com.yaho.factchecker.global.type.ClaimCategory;
+import lombok.Builder;
 
+@Builder
 public record ClaimCategoryCreateCommand(
         ClaimCategory category,
         boolean primaryCategory
--- src/main/java/com/yaho/factchecker/domain/claim/dto/command/ClaimCreateCommand.java
+++ src/main/java/com/yaho/factchecker/domain/claim/dto/command/ClaimCreateCommand.java
@@ -2,7 +2,9 @@ package com.yaho.factchecker.domain.claim.dto.command;
 
 import java.util.List;
 import java.util.UUID;
+import lombok.Builder;
 
+@Builder
 public record ClaimCreateCommand(
         UUID factCheckId,
         UUID claimAnalysisAiLogId,
